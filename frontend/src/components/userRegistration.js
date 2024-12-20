332e84bb4f623b9ade2dfde7ce3cdbb332931a67
Tharun <tharunkudikyala@.com>
2024-12-20 19:44:18 +0530

    user registration fixed

--- frontend/src/components/userRegistration.js
+++ frontend/src/components/userRegistration.js
@@ -1,7 +1,272 @@
-import React, { useEffect, useState } from "react";
+// import React, { useEffect, useState } from "react";
+// import React, { useEffect, useState } from "react";
+// import { useNavigate } from "react-router-dom";
+// import "./UserRegistration.css"; // Ensure this CSS file is imported for styling
+
+// const UserRegistration = () => {
+//   const [latitude, setLatitude] = useState(null);
+//   const [longitude, setLongitude] = useState(null);
+//   const [error, setError] = useState(null);
+//   const [isLoading, setIsLoading] = useState(false);
+//   const [isSendingOtp, setIsSendingOtp] = useState(false);
+//   const [latitude, setLatitude] = useState(null);
+//   const [longitude, setLongitude] = useState(null);
+//   const [error, setError] = useState(null);
+//   const [isLoading, setIsLoading] = useState(false);
+//   const [isSendingOtp, setIsSendingOtp] = useState(false);
+//   const [formData, setFormData] = useState({
+//     name: "",
+//     name: "",
+//     email: "",
+//     phone: "",
+//     address: "",
+//   });
+//   const [otp, setOtp] = useState("");
+//   const [isOtpSent, setIsOtpSent] = useState(false);
+//   const navigate = useNavigate();
+
+//   const handleChange = (e) => {
+//     setFormData({ ...formData, [e.target.name]: e.target.value });
+//   };
+
+//   const handleSubmit = async (e) => {
+//     e.preventDefault();
+//     setIsSendingOtp(true);
+//     setIsSendingOtp(true);
+//     try {
+//       const response = await fetch(
+//         "http://localhost:9004/api/otp",
+//         {
+//           method: "POST",
+//           headers: {
+//             "Content-Type": "application/json",
+//           },
+//           body: JSON.stringify(formData),
+//         }
+//       );
+//       setIsSendingOtp(false);
+//       if (response.status === 200) {
+//       const response = await fetch(
+//         "http://localhost:9004/api/otp",
+//         {
+//           method: "POST",
+//           headers: {
+//             "Content-Type": "application/json",
+//           },
+//           body: JSON.stringify(formData),
+//         }
+//       );
+//       setIsSendingOtp(false);
+//       if (response.status === 200) {
+//         setIsOtpSent(true);
+//       } else {
+//         alert("Error sending OTP");
+//       }
+//     } catch (error) {
+//       setIsSendingOtp(false);
+//       setIsSendingOtp(false);
+//       console.error("Error:", error);
+//       alert("Error sending OTP");
+//     }
+//   };
+
+//   // useEffect(() => {
+//   //   const getLocation = () => {
+//   //     setIsLoading(true);
+//   //     if (navigator.geolocation) {
+//   //       navigator.geolocation.getCurrentPosition(
+//   //         (position) => {
+//   //           setLatitude(position.coords.latitude);
+//   //           setLongitude(position.coords.longitude);
+//   //           setError(null);
+//   //           setIsLoading(false);
+//   //         },
+//   //         (error) => {
+//   //           setError(error.message);
+//   //           setIsLoading(false);
+//   //         },
+//   //         {
+//   //           enableHighAccuracy: true,
+//   //           timeout: 10000,
+//   //           maximumAge: 0,
+//   //         }
+//   //       );
+//   //     } else {
+//   //       setError("Geolocation is not supported by your browser.");
+//   //       setIsLoading(false);
+//   //     }
+//   //   };
+//   //   getLocation();
+//   // }, []);
+
+//   useEffect(() => {
+//     const getLocation = () => {
+//       setIsLoading(true);
+//       if (navigator.geolocation) {
+//         navigator.geolocation.getCurrentPosition(
+//           (position) => {
+//             setLatitude(position.coords.latitude);
+//             setLongitude(position.coords.longitude);
+//             setError(null);
+//             setIsLoading(false);
+//           },
+//           (error) => {
+//             setError(error.message);
+//             setIsLoading(false);
+//           },
+//           {
+//             enableHighAccuracy: true,
+//             timeout: 10000,
+//             maximumAge: 0,
+//           }
+//         );
+//       } else {
+//         setError("Geolocation is not supported by your browser.");
+//         setIsLoading(false);
+//       }
+//     };
+//     getLocation();
+//   }, []);
+
+//   const handleOtpSubmit = async (e) => {
+//     e.preventDefault();
+//     try {
+//       let response = await fetch(
+//         "http://localhost:9004/api/otpVerify",
+//         {
+//           method: "POST",
+//           headers: {
+//             "Content-Type": "application/json",
+//           },
+//           body: JSON.stringify({ email: formData.email, otp }),
+//         }
+//       );
+
+//       if (response.ok) {
+//         const loca = { lat: latitude, long: longitude };
+
+//         response = await fetch(
+//           "http://localhost:9004/api/auth/register",
+//           {
+//             method: "POST",
+//             headers: {
+//               "Content-Type": "application/json",
+//             },
+//             body: JSON.stringify({ ...formData, ...loca }),
+//           }
+//         );
+
+//         if (response.ok) {
+//           navigate("/login");
+//         } else {
+//           const result = await response.json();
+//           alert(result.msg || "Error creating user");
+//         }
+//       } else {
+//         alert("Invalid OTP");
+//       }
+//     } catch (error) {
+//       console.error("Error:", error);
+//       alert("Error validating OTP");
+//     }
+//   };
+
+//   return (
+//     <div className="registration-form-container">
+//       {!isOtpSent ? (
+//         <>
+//           {isSendingOtp ? (
+//             <div className="loading-message">
+//               <div className="spinner"></div>
+//               <h2>⏳ Just a Moment!</h2>
+//               <p>
+//                 Good things take time. Your OTP is on its way and will arrive
+//                 shortly...
+//               </p>
+
+//               <p>Your patience is appreciated!</p>
+//               <button
+//                 className="refresh-button"
+//                 onClick={() => window.location.reload()}
+//               >
+//                 Refresh
+//               </button>
+//             </div>
+//           ) : (
+//               <form onSubmit={handleSubmit} className="registration-form">
+//                 <h2>User Registration</h2>
+//                 <label>
+//                   Organisation Name / Name:
+//                   <input
+//                     type="text"
+//                     placeholder="Enter Your Name"
+//                     name="name"
+//                     value={formData.name}
+//                     onChange={handleChange}
+//                     required
+//                   />
+//                 </label>
+//                 <label>
+//                   Email:
+//                   <input
+//                     type="email"
+//                     placeholder="Enter Your Email"
+//                     name="email"
+//                     value={formData.email}
+//                     onChange={handleChange}
+//                     required
+//                   />
+//                 </label>
+//                 <label>
+//                   Phone:
+//                   <input
+//                     type="text"
+//                     placeholder="Enter Your Mobile Number"
+//                     name="phone"
+//                     value={formData.phone}
+//                     onChange={handleChange}
+//                     required
+//                   />
+//                 </label>
+//                 <label>
+//                   Address:
+//                   <input
+//                     type="text"
+//                     placeholder="Enter Your Address"
+//                     name="address"
+//                     value={formData.address}
+//                     onChange={handleChange}
+//                     required
+//                   />
+//                 </label>
+//                 <button type="submit">Register</button>
+//               </form>
+//           )}
+//         </>
+//       ) : (
+//         <form onSubmit={handleOtpSubmit} className="otp-form">
+//           <label>
+//             Enter OTP:
+//             <input
+//               type="text"
+//               name="otp"
+//               value={otp}
+//               onChange={(e) => setOtp(e.target.value)}
+//               required
+//             />
+//           </label>
+//           <button type="submit">Submit OTP</button>
+//         </form>
+//       )}
+//     </div>
+//   );
+// };
+
+// export default UserRegistration;
+
 import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
-import "./UserRegistration.css"; // Ensure this CSS file is imported for styling
+import "./UserRegistration.css";
 
 const UserRegistration = () => {
   const [latitude, setLatitude] = useState(null);
@@ -9,13 +274,7 @@ const UserRegistration = () => {
   const [error, setError] = useState(null);
   const [isLoading, setIsLoading] = useState(false);
   const [isSendingOtp, setIsSendingOtp] = useState(false);
-  const [latitude, setLatitude] = useState(null);
-  const [longitude, setLongitude] = useState(null);
-  const [error, setError] = useState(null);
-  const [isLoading, setIsLoading] = useState(false);
-  const [isSendingOtp, setIsSendingOtp] = useState(false);
   const [formData, setFormData] = useState({
-    name: "",
     name: "",
     email: "",
     phone: "",
@@ -32,31 +291,17 @@ const UserRegistration = () => {
   const handleSubmit = async (e) => {
     e.preventDefault();
     setIsSendingOtp(true);
-    setIsSendingOtp(true);
     try {
-      const response = await fetch(
-        "http://localhost:9004/api/otp",
-        {
-          method: "POST",
-          headers: {
-            "Content-Type": "application/json",
-          },
-          body: JSON.stringify(formData),
-        }
-      );
+      const response = await fetch("http://localhost:9004/api/otp", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(formData),
+      });
+
       setIsSendingOtp(false);
-      if (response.status === 200) {
-      const response = await fetch(
-        "http://localhost:9004/api/otp",
-        {
-          method: "POST",
-          headers: {
-            "Content-Type": "application/json",
-          },
-          body: JSON.stringify(formData),
-        }
-      );
-      setIsSendingOtp(false);
+
       if (response.status === 200) {
         setIsOtpSent(true);
       } else {
@@ -64,74 +309,15 @@ const UserRegistration = () => {
       }
     } catch (error) {
       setIsSendingOtp(false);
-      setIsSendingOtp(false);
       console.error("Error:", error);
       alert("Error sending OTP");
     }
   };
 
-  useEffect(() => {
-    const getLocation = () => {
-      setIsLoading(true);
-      if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(
-          (position) => {
-            setLatitude(position.coords.latitude);
-            setLongitude(position.coords.longitude);
-            setError(null);
-            setIsLoading(false);
-          },
-          (error) => {
-            setError(error.message);
-            setIsLoading(false);
-          },
-          {
-            enableHighAccuracy: true,
-            timeout: 10000,
-            maximumAge: 0,
-          }
-        );
-      } else {
-        setError("Geolocation is not supported by your browser.");
-        setIsLoading(false);
-      }
-    };
-    getLocation();
-  }, []);
-
-  useEffect(() => {
-    const getLocation = () => {
-      setIsLoading(true);
-      if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(
-          (position) => {
-            setLatitude(position.coords.latitude);
-            setLongitude(position.coords.longitude);
-            setError(null);
-            setIsLoading(false);
-          },
-          (error) => {
-            setError(error.message);
-            setIsLoading(false);
-          },
-          {
-            enableHighAccuracy: true,
-            timeout: 10000,
-            maximumAge: 0,
-          }
-        );
-      } else {
-        setError("Geolocation is not supported by your browser.");
-        setIsLoading(false);
-      }
-    };
-    getLocation();
-  }, []);
-
   const handleOtpSubmit = async (e) => {
     e.preventDefault();
     try {
-      let response = await fetch(
+      const verifyResponse = await fetch(
         "http://localhost:9004/api/otpVerify",
         {
           method: "POST",
@@ -142,10 +328,10 @@ const UserRegistration = () => {
         }
       );
 
-      if (response.ok) {
+      if (verifyResponse.ok) {
         const loca = { lat: latitude, long: longitude };
 
-        response = await fetch(
+        const registerResponse = await fetch(
           "http://localhost:9004/api/auth/register",
           {
             method: "POST",
@@ -156,10 +342,10 @@ const UserRegistration = () => {
           }
         );
 
-        if (response.ok) {
+        if (registerResponse.ok) {
           navigate("/login");
         } else {
-          const result = await response.json();
+          const result = await registerResponse.json();
           alert(result.msg || "Error creating user");
         }
       } else {
@@ -171,6 +357,35 @@ const UserRegistration = () => {
     }
   };
 
+  useEffect(() => {
+    const getLocation = () => {
+      setIsLoading(true);
+      if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(
+          (position) => {
+            setLatitude(position.coords.latitude);
+            setLongitude(position.coords.longitude);
+            setError(null);
+            setIsLoading(false);
+          },
+          (error) => {
+            setError(error.message);
+            setIsLoading(false);
+          },
+          {
+            enableHighAccuracy: true,
+            timeout: 10000,
+            maximumAge: 0,
+          }
+        );
+      } else {
+        setError("Geolocation is not supported by your browser.");
+        setIsLoading(false);
+      }
+    };
+    getLocation();
+  }, []);
+
   return (
     <div className="registration-form-container">
       {!isOtpSent ? (
@@ -179,68 +394,57 @@ const UserRegistration = () => {
             <div className="loading-message">
               <div className="spinner"></div>
               <h2>⏳ Just a Moment!</h2>
-              <p>
-                Good things take time. Your OTP is on its way and will arrive
-                shortly...
-              </p>
-
-              <p>Your patience is appreciated!</p>
-              <button
-                className="refresh-button"
-                onClick={() => window.location.reload()}
-              >
-                Refresh
-              </button>
+              <p>Good things take time. Your OTP is on its way...</p>
             </div>
           ) : (
-              <form onSubmit={handleSubmit} className="registration-form">
-                <h2>User Registration</h2>
-                <label>
-                  Organisation Name / Name:
-                  <input
-                    type="text"
-                    placeholder="Enter Your Name"
-                    name="name"
-                    value={formData.name}
-                    onChange={handleChange}
-                    required
-                  />
-                </label>
-                <label>
-                  Email:
-                  <input
-                    type="email"
-                    placeholder="Enter Your Email"
-                    name="email"
-                    value={formData.email}
-                    onChange={handleChange}
-                    required
-                  />
-                </label>
-                <label>
-                  Phone:
-                  <input
-                    type="text"
-                    placeholder="Enter Your Mobile Number"
-                    name="phone"
-                    value={formData.phone}
-                    onChange={handleChange}
-                    required
-                  />
-                </label>
-                <label>
-                  Address:
-                  <input
-                    type="text"
-                    placeholder="Enter Your Address"
-                    name="address"
-                    value={formData.address}
-                    onChange={handleChange}
-                    required
-                  />
-                </label>
-                <button type="submit">Register</button>
-              </form>
+            <form onSubmit={handleSubmit} className="registration-form">
+              <h2>User Registration</h2>
+              <label>
+                Name:
+                <input
+                  type="text"
+                  placeholder="Enter Your Name"
+                  name="name"
+                  value={formData.name}
+                  onChange={handleChange}
+                  required
+                />
+              </label>
+              <label>
+                Email:
+                <input
+                  type="email"
+                  placeholder="Enter Your Email"
+                  name="email"
+                  value={formData.email}
+                  onChange={handleChange}
+                  required
+                />
+              </label>
+              <label>
+                Phone:
+                <input
+                  type="text"
+                  placeholder="Enter Your Mobile Number"
+                  name="phone"
+                  value={formData.phone}
+                  onChange={handleChange}
+                  required
+                />
+              </label>
+              <label>
+                Address:
+                <input
+                  type="text"
+                  placeholder="Enter Your Address"
+                  name="address"
+                  value={formData.address}
+                  onChange={handleChange}
+                  required
+                />
+              </label>
+              <button type="submit">Register</button>
+            </form>
           )}
         </>
       ) : (
